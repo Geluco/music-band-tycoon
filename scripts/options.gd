ad711f90fdaaa956587b8afa34b9a3223244f087
UsualNonChalant <pablolorenzog06@gmail.com>
2025-08-27 16:07:44 +0200

    fin opciones y menu
    finiquitado opciones y menu, arreglar resolucion

--- scripts/options.gd
+++ scripts/options.gd
@@ -7,3 +7,17 @@ func _on_h_slider_value_changed(value: float) -> void:
 
 func _on_check_box_toggled(toggled_on: bool) -> void:
 	AudioServer.set_bus_mute(0,toggled_on)
+
+
+func _on_option_button_item_selected(index: int) -> void:
+	match  index:
+		0:
+			DisplayServer.window_set_size(Vector2i(1920,1080))
+		1:
+			DisplayServer.window_set_size(Vector2i(1600,900))
+		2:
+			DisplayServer.window_set_size(Vector2i(1280,720))
+
+
+func _on_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/menu.tscn")
